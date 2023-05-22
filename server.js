9d9bc7f119428b42498b2e73fdfc351b264c0f73
dirga89 <dirga89@gmail.com>
2023-05-22 19:31:04 +0700

    --package

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
 const express = require('express');
 const bcrypt = require('bcrypt');
-//const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const knex = require('knex');
 
